3c4becfdeb04f1ac130cb66348a9a842ccf7e109
DESKTOP-MOA5RPK\EBLEE <wiseeunbi1@gmail.com>
2025-11-13 14:00:36 +0900

    [이은비] - 회사컴 수정사항 반영

--- src/main/java/com/eunbi/springtest/database/controller/UsedGoodsController.java
+++ src/main/java/com/eunbi/springtest/database/controller/UsedGoodsController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import java.util.List;  
 
 // Request, Response 관련 기능 수행까지만 controller에서 정의
 
